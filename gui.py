11895cc14032a3d26312bed65038b23df13e7658
Cate <dchcpw@gmail.com>
2019-08-06 20:05:50 -0400

    added mp3 conversion!!!

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ from appJar import gui
 import sys
 from shutil import rmtree
 import os
+import ffmpeg
 
 # import functions
 from funcs import beatsReversed
@@ -22,6 +23,7 @@ def function(file):
     upload = data['f1']
     ytLink = data['link']
     output = data['output'] + '/output.wav'
+    fileType = data['type']
     
     importedFile = None
 
@@ -49,27 +51,51 @@ def function(file):
 
     if selection == "Reverse beats of the audio file.":
         beatsReversed.run(importedFile, output)
+        print('Starting proccess...')
     if selection == "Get every 1st and 3rd beat of the audio file.":
         everyOddBeat.run(importedFile, output)
+        print('Starting proccess...')
     if selection == "Get every 2nd and 4th beat of the audio file.":
         everyEvenBeat.run(importedFile, output)
+        print('Starting proccess...')
     if selection == "Get every 1st beat of the audio file.":
         everyFirstBeat.run(importedFile, output)
+        print('Starting proccess...')
     if selection == "Get every 2nd beat of the audio file.":
         everySecondBeat.run(importedFile, output)
+        print('Starting proccess...')
     if selection == "Get every 3rd beat of the audio file.":
         everyThirdBeat.run(importedFile, output)
+        print('Starting proccess...')
     if selection == "Get every 4th beat of the audio file.":
         everyFourthBeat.run(importedFile, output)
+        print('Starting proccess...')
 
     if os.path.exists('./tmp') and os.path.isdir('./tmp'):
+        print('Deleting temp directory...')
         rmtree('./tmp')
+        print('Deleted!')
+    
+    if fileType == '.mp3 (slower, much smaller file size)':
+        print('MP3 conversion starting...')
+        {
+            ffmpeg
+            .input(output)
+            .output('output.mp3', acodec='libmp3lame')
+            .run()
+        }
+        print('MP3 conversion complete!')
+        if os.path.exists(output):
+            os.remove(output)
+            print('Deleted .wav file.')
 
+
+    print('Success!')
     app.infoBox("Success!", "Proccess complete! Check your output directory.", parent=None)
 
 
 # create a GUI variable called app
-app = gui("Amen Audio Tools", "400x300")
+app = gui("Amen Audio Tools", "400x400")
 
 app.setIcon('./resources/icon.gif')
 app.setResizable(canResize=False)
@@ -94,6 +120,10 @@ app.addEntry("link")
 
 app.addLabel("outputLabel", "Where to save the output?")
 app.addDirectoryEntry("output")
+
+app.addLabel("whatType", "How about file type?")
+app.addRadioButton("type", ".wav (faster, much larger file size)")
+app.addRadioButton("type", ".mp3 (slower, much smaller file size)")
 
 app.addButton("Perform Magic", function)
 
